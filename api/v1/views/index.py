d19fa1c136d24c7104f56840d771a12e433ebd32
derrickric <derrickmbaka@gmail.com>
2023-02-17 15:03:14 -0800

    documenting the code

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,7 +10,21 @@ from models import storage
 @app_views.route('/status', methods=['GET'])
 def status():
     """
-    function for status route that returns the status
+    Returns a JSON response containing the status of the API.
+
+    HTTP Method:
+      - GET
+
+    Returns:
+      - A JSON object with a single "status" key set to "OK".
+
+    Example:
+      ```
+      >>> import requests
+      >>> response = requests.get('http://localhost:5000/api/v1/status')
+      >>> response.json()
+      {'status': 'OK'}
+      ```
     """
     if request.method == 'GET':
         resp = {"status": "OK"}
@@ -20,7 +34,28 @@ def status():
 @app_views.route('/stats', methods=['GET'])
 def stats():
     """
-    function to return the count of all class objects
+    Returns a JSON response containing the count of all objects by class.
+
+    HTTP Method:
+      - GET
+
+    Returns:
+      - A JSON object with keys for each class name and values for the count of objects of that class.
+
+    Example:
+      ```
+      >>> import requests
+      >>> response = requests.get('http://localhost:5000/api/v1/stats')
+      >>> response.json()
+      {
+          'amenities': 10,
+          'cities': 5,
+          'places': 20,
+          'reviews': 15,
+          'states': 2,
+          'users': 100
+      }
+      ```
     """
     if request.method == 'GET':
         response = {}
@@ -35,3 +70,4 @@ def stats():
         for key, value in PLURALS.items():
             response[value] = storage.count(key)
         return jsonify(response)
+
